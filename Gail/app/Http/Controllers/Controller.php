ba408d0667dfd40ef2dd13560a8da4dfefbfdcb7
Justine Gail Valdez <44793489+Gaileeee08@users.noreply.github.com>
2025-01-16 09:48:58 +0000

    Controller added

--- Gail/app/Http/Controllers/Controller.php
+++ Gail/app/Http/Controllers/Controller.php
@@ -1,6 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
+class GreetController extends Controller
+{
+    public function show(){
+    
+       
+     
+         
+     return view('greet');
+    }
+}
+
 
 abstract class Controller
 {
