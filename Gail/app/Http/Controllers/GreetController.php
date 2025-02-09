691434edb40c8f88543b8b1dadfa76f4fa6f9fc8
Justine Gail Valdez <44793489+Gaileeee08@users.noreply.github.com>
2025-02-10 04:24:46 +0800

    Hello and Greet FINALLY CORRECTED AND WORKING

--- Gail/app/Http/Controllers/GreetController.php
+++ Gail/app/Http/Controllers/GreetController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class GreetController extends Controller
 {
     public function show()
     {
-        return view('greet'); // This loads resources/views/greet.blade.php
+        return view('user'); // This loads resources/views/greet.blade.php
     }
 }
